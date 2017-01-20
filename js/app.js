739ad7610f477e71a162caa4dbc2bc5831e5e64f
Zak <zakattack26111@gmail.com>
2017-01-20 13:57:08 -1000

    fixed transform property problem

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
 function clickMe(){
   var elementId = document.getElementById("jsEvent");
-  elementId.style.tranform = "translate(20px, 0px)";
+  elementId.style.transform = "translate(200px, 0px)";
 }
